15c324841514c58058a3b51617fc635c96682f73
amin-newcastle <muhammad-karim@hotmail.com>
2021-09-27 15:36:02 +0100

    created Oyster class and new card

---
+++ lib/oystercard.rb
@@ -0,0 +1,7 @@
+class Oystercard
+    def initialize
+        @new_card = 0
+    end
+
+    attr_reader :new_card
+end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,3 +1,7 @@
-describe OysterCard do
-  
+require 'oystercard'
+
+describe Oystercard do
+    it "Should create a blank Oyster card" do
+        expect(subject.new_card).to eq (0)
+    end  
 end
