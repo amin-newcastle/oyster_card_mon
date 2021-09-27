4e687e583371c1a324fd412ee78d37ea78bffe31
amin-newcastle <muhammad-karim@hotmail.com>
2021-09-27 16:39:07 +0100

    Added error in top_up with MAX_CARD_LIMIT

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,17 @@ class Oystercard
 
     def initialize
         @balance = 0
+        @MAX_CARD_LIMIT = 90
     end
 
-    attr_reader :balance
+    attr_reader :balance, :MAX_CARD_LIMIT
 
     def top_up(amount)
-        @balance += amount
+        if @balance + amount < @MAX_CARD_LIMIT
+            @balance += amount
+        else
+            p "Maximum balance exceeded of £#{@MAX_CARD_LIMIT}"
+        end
     end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -6,7 +6,12 @@ describe Oystercard do
         expect(subject.balance).to eq (0)
     end
 
-    it 'Should add an amount to an Oystercard' do
-      expect(subject.top_up(100)).to eq(subject.balance)
+    it "Should add an amount to an Oystercard" do
+        expect(subject.top_up(50)).to eq(subject.balance)
     end
+    
+    it "Should return an error if limit exceeded" do
+        expect(subject.top_up(100)).to eq("Maximum balance exceeded of £#{subject.MAX_CARD_LIMIT}")
+    end
+
 end
